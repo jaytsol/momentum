1d7c32e9ff26eb83e3ff170386abc7936f46f312
jaytsol <jaytsol@naver.com>
2025-02-27 02:22:49 +0900

    login

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
+const greeting = document.querySelector("#greeting");
 
-function onLoginSubmit() {
-    console.log(loginInput.value);
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
-loginButton.addEventListener("click", onLoginSubmit);
+loginForm.addEventListener("submit", onLoginSubmit);
+
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+} else {
+    paintGreetings(savedUsername);
+}
