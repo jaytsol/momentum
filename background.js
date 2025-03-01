4513738e5f3e455863ecb95f7011c6bbaed56efa
jaytsol <jaytsol@naver.com>
2025-03-01 17:33:11 +0900

    added image

---
+++ background.js
@@ -0,0 +1,9 @@
+const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+
+const todaysImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${todaysImage}`;
+
+document.body.appendChild(bgImage);
