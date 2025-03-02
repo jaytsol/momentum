44cf5c6e7c74d8a6e1662c27fa5db0b33b8250e0
jaytsol <jaytsol@naver.com>
2025-03-02 18:47:25 +0900

    added delete feature

--- todo.js
+++ todo.js
@@ -2,20 +2,36 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
-const paintToDo = (newToDo) => {
+const renderToDo = (newToDo) => {
     const li = document.createElement("li");
-    const span = document.createElement("span");
-    li.appendChild(span);
-    span.innerText = newToDo;
+    renderSpan(newToDo, li);
+    renderButton(li);
     toDoList.appendChild(li);
+}
+
+const renderSpan = (newToDo, li) => {
+    const span = document.createElement("span");
+    span.innerText = newToDo;
+    li.appendChild(span);
+}
+
+const renderButton = (li) => {
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", handleToDoDelete);
+    li.appendChild(button);
+}
+
+const handleToDoDelete = (event) => {
+    const li = event.target.parentElement;
+    li.remove();
 }
 
 const handleToDoSubmit = (event) => {
     event.preventDefault();
-    console.log(todoInput.value);
     const newToDo = todoInput.value;
     todoInput.value = "";
-    paintToDo(newToDo);
+    renderToDo(newToDo);
 }
 
 todoForm.addEventListener("submit", handleToDoSubmit);
