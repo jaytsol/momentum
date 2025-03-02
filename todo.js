faeba1b34ce22ce3e5087fed951ede234a671a86
jaytsol <jaytsol@naver.com>
2025-03-02 18:32:14 +0900

    added toDoList

--- todo.js
+++ todo.js
@@ -2,11 +2,20 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
+const paintToDo = (newToDo) => {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newToDo;
+    toDoList.appendChild(li);
+}
+
 const handleToDoSubmit = (event) => {
     event.preventDefault();
     console.log(todoInput.value);
-    const newTodo = todoInput.value;
+    const newToDo = todoInput.value;
     todoInput.value = "";
+    paintToDo(newToDo);
 }
 
 todoForm.addEventListener("submit", handleToDoSubmit);
