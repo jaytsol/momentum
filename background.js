f0b7bc0fd0f3cd5528db6a1a46493cc1d7f38d90
jaytsol <jaytsol@naver.com>
2025-03-03 17:38:27 +0900

    changed background and css

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const images = ["0.jpeg", "1.jpeg", "2.webp", "3.webp", "4.jpg"];
 
 const todaysImage = images[Math.floor(Math.random() * images.length)];
 
